b55dea9e69544ae4e961f6c34310f3fc1ae570ca
dtsivkovski <dtsivkovs@gmail.com>
2025-04-29 19:01:18 -0700

    Initial Commit

---
+++ Coffee Rate/Coffee_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_RateApp.swift
+//  Coffee Rate
+//
+//  Created by Daniel Tsivkovski on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee RateTests/Coffee_RateTests.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_RateTests.swift
+//  Coffee RateTests
+//
+//  Created by Daniel Tsivkovski on 4/29/25.
+//
+
+import Testing
+@testable import Coffee_Rate
+
+struct Coffee_RateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
