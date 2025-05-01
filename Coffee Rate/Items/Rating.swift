05f3cdb605d99e5bb5ab88241e4e888706b4e3d1
dtsivkovski <dtsivkovs@gmail.com>
2025-05-01 10:26:08 -0700

    added Rating class

--- Coffee Rate/Items/Rating.swift
+++ Coffee Rate/Items/Rating.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import SwiftData
+import MapKit
+
+enum NoiseLevel {
+    case quiet;
+    case normal;
+    case loud;
+}
+
+@Model
+class Rating: Identifiable {
+    var id: UUID = UUID();
+    
+    // Coffee Shop Data
+    var name: String;
+    var location: CLLocationCoordinate2D?;
+    var whenVisited: Date;
+    
+    // Rating Information
+    var studyVibe: Int;
+    var foodOrDrinkRating: Int;
+    var noiseLevel: NoiseLevel;
+    var availability: Int;
+    var overallRating: Int;
+    var comments: String?;
+    
+    // initializer
+    init(
+        id: UUID = UUID(),
+        name: String,
+        location: CLLocationCoordinate2D? = nil,
+        whenVisited: Date,
+        studyVibe: Int,
+        foodOrDrinkRating: Int,
+        noiseLevel: NoiseLevel,
+        availability: Int,
+        overallRating: Int,
+        comments: String? = nil
+    ) {
+        self.id = id;
+        self.name = name;
+        self.location = location;
+        self.whenVisited = whenVisited;
+        self.studyVibe = studyVibe;
+        self.foodOrDrinkRating = foodOrDrinkRating;
+        self.noiseLevel = noiseLevel;
+        self.availability = availability;
+        self.overallRating = overallRating;
+        self.comments = comments;
+    }
+}
