241c5b267f9cf29f40d43c226cdba7fe681e85e7
dtsivkovski <dtsivkovs@gmail.com>
2025-05-03 18:31:26 -0700

    added better visual layout for details, added user notes section

--- Coffee Rate/Items/RatingDetails.swift
+++ Coffee Rate/Items/RatingDetails.swift
@@ -12,6 +12,7 @@ import MapKit
 struct RatingDetails: View {
     
     @Environment(\.modelContext) var modelContext;
+    @Environment(\.colorScheme) var colorScheme;
     @Query var ratings: [Rating];
     var rating : Rating;
     
@@ -24,17 +25,26 @@ struct RatingDetails: View {
     // get a string for the noise level value
     var noiseLevelString: String {
         switch (NoiseLevel(rawValue: rating.noiseLevel)) {
-        case .quiet:
-            return "Quiet";
-        case .loud:
-            return "Loud";
-        default:
-            return "Normal";
+            case .quiet:
+                return "Quiet";
+            case .loud:
+                return "Loud";
+            default:
+                return "Normal";
         }
     }
     
     var circleBackgroundColor : Color {
         get {
+            if (colorScheme == .dark) {
+                if (rating.overallRating > 7.0) {
+                    return Color(red: 50/256, green: 150/256, blue: 50/256)
+                } else if (rating.overallRating < 3.5) {
+                    return Color(red: 150/256, green: 50/256, blue: 50/256)
+                } else {
+                    return .yellow.mix(with: .red, by: 0.3).mix(with: .black, by: 0.05);
+                }
+            }
             if (rating.overallRating > 7.0) {
                 return Color(red: 213/256, green: 252/256, blue: 192/256)
             } else if (rating.overallRating < 3.5) {
@@ -76,7 +86,7 @@ struct RatingDetails: View {
                 Text("\(rating.overallRating.formatted())")
                     .font(.title)
                     .fontWeight(.semibold)
-                    .shadow(radius:1, y:2)
+                    .shadow(radius:1, y: 2)
             }
             .offset(y: -80)
             .padding([.bottom], -70)
@@ -84,46 +94,73 @@ struct RatingDetails: View {
                 Text(rating.name)
                     .font(.title)
                     .fontWeight(.bold)
-                RatingProgress(label: "Study Vibe", value: rating.studyVibe, outOf: 10)
-                RatingProgress(label: "Food/Drink", value: rating.foodOrDrinkRating, outOf: 10)
-                RatingProgress(label: "Availability", value: rating.availability, outOf: 5)
-                VStack {
-                    HStack {
-                        Text("Noise Level")
-                            .fontWeight(.semibold)
-                        Spacer()
-                        Text(noiseLevelString)
-                            .fontWeight(.semibold)
+                Text("Visited on \(rating.whenVisited, style: .date)")
+                    .font(.subheadline)
+                    .padding(.bottom, 8)
+                // z stack containing background rectangle and the individual ratings
+                ZStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.thinMaterial)
+                        .shadow(radius: 8, y: 5.0)
+                    VStack {
+                        HStack {
+                            Text("Ratings")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }.padding(-3)
+                        RatingProgress(symbol: "laptopcomputer" ,label: "Study Vibe", value: rating.studyVibe, outOf: 10)
+                        RatingProgress(symbol: "cup.and.saucer.fill", label: "Food/Drink", value: rating.foodOrDrinkRating, outOf: 10)
+                        RatingProgress(symbol: "person.2.fill", label: "Availability", value: rating.availability, outOf: 5)
+                        RatingProgress(symbol: "waveform", label: "Noise Level", value: rating.noiseLevel, outOf: 2, alternateLabel: noiseLevelString)
                     }
-                    .padding(.bottom, -2)
-                    ProgressView(value: Double(rating.noiseLevel)/2.0)
+                    .padding(16)
                 }
-                .padding(.top, 10)
+                if (rating.comments != nil) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.thinMaterial)
+                            .shadow(radius: 8, y: 5.0)
+                        VStack {
+                            HStack {
+                                Text("Notes")
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                            }.padding(-2)
+                            Text(rating.comments!)
+                        }
+                            .padding(16)
+                    }.padding(.top, 10)
+                }
+                
             }
-            .padding([.leading, .trailing], 40)
+            .padding([.leading, .trailing], 20)
         }
     }
 }
 
 struct RatingProgress : View {
     
+    var symbol: String;
     var label: String;
     
     var value: Int;
     var outOf: Int;
     
+    var alternateLabel: String?;
+    
     var body: some View {
         VStack {
             HStack {
+                Image(systemName: symbol)
                 Text(label)
-                    .fontWeight(.semibold)
+                    .fontWeight(.medium)
                 Spacer()
-                Text("\(value)/\(outOf)")
+                Text( alternateLabel != nil ? alternateLabel! : "\(value)/\(outOf)")
                     .fontWeight(.semibold)
             }.padding(.bottom, -2)
             ProgressView(value: Double(value)/Double(outOf))
         }
-        .padding(.top, 10)
+        .padding([.top, .bottom], 5)
     }
 }
 
@@ -147,8 +184,10 @@ struct MapPreview : View {
             // marker for the coffee shop
             Marker(
                 name,
-                coordinate: location)
+                coordinate: location
+            )
         }
+        .mapStyle(.standard(pointsOfInterest: .excludingAll))
         .frame(height: 300)
         .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
         .padding(20)
@@ -157,5 +196,5 @@ struct MapPreview : View {
 }
 
 #Preview {
-    RatingDetails(rating: Rating(name: "Philz Coffee", latitude: 33.789955, longitude: -117.853434, whenVisited: Date(), studyVibe: 9, foodOrDrinkRating: 9, noiseLevel: .normal, availability: 2, overallRating: 8.7))
+    RatingDetails(rating: Rating(name: "Philz Coffee", latitude: 33.789955, longitude: -117.853434, whenVisited: Date(), studyVibe: 9, foodOrDrinkRating: 9, noiseLevel: .normal, availability: 2, overallRating: 5.4, comments: "Really amazing place for a coffee! Good drinks, even better atmosphere, but not really ideal spots to actually study at."))
 }
